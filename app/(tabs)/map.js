5940a837d1d9f840d2a406f1e08a0eca6fdc60f8
swagfather808 <rkringor@gmail.com>
2025-09-23 14:11:29 -1000

    9/23

---
+++ app/(tabs)/map.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import LinkButton from '../../components/LinkButton';
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>Map</Text>
+      <LinkButton page="/" title= "Go back to home page"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#df4eb4ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- app/index.js
+++ app/index.js
@@ -1,13 +1,44 @@
 import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
 import LinkButton from '../components/LinkButton';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+  
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+          <Text style={styles.text}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
       <Text>Main Page</Text>
       <LinkButton page="about" title= "Go to about page"/>
       <LinkButton page="contact" title= "Go to contact page"/>
+      <LinkButton page="profile" title= "Go to profile page"/>
     </View>
   );
 }
@@ -18,5 +49,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
